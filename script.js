c5d2aafb60e8ea186014f53e446ee6289922c606
cassianobender <bender.c92@gmail.com>
2023-08-27 02:28:30 -0300

    mediaqueries Login

---
+++ script.js
@@ -0,0 +1,17 @@
+function clicar(){
+    var form = document.getElementById('iuser')
+    if(form.style.backgroundColor != 'lightgreen'){
+        form.style.backgroundColor = 'lightgreen'
+    }else{
+        form.style.backgroundColor = 'white'
+    }
+}
+
+function clicar2(){
+    var form2 = document.getElementById('ipass')
+    if(form2.style.backgroundColor != 'lightgreen'){
+        form2.style.backgroundColor = 'lightgreen'
+    }else{
+        form2.style.backgroundColor = 'white'
+    }
+}
